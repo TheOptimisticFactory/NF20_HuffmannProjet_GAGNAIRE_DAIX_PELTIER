0fe25a80470daad2544097e74c56ceb6a58bab66
Romain GAGNAIRE <lil_rom@orange.fr>
2013-11-16 11:22:56 +0100

    Ajout de classes basiques

---
+++ NF20_HuffmannProject_Gagnaire_Daix_Peltier/src/fileProcessing/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author JoeTheFuckingFrypan
+ *
+ */
+package fileProcessing;
---
+++ NF20_HuffmannProject_Gagnaire_Daix_Peltier/src/mainPackage/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package principal contenant le point d'entrée du programme 
+ */
+
+/**
+ * @author JoeTheFuckingFrypan
+ * @version 0.1
+ */
+package mainPackage;

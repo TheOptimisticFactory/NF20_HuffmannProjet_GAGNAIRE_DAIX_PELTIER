e8229b3369f689eed6cdda79676ca5849089d184
ShadowsKiller <lil_rom@orange.fr>
2013-12-27 21:45:51 +0100

    Ajout du benchmark de décodage
    Pour simplifier les choses --n'étant pas à 100% sûr du fonction de la
    méthode setUp(), les fichiers .bin doivent être présents pour que les
    tests fonctionne.
    
    Pour les créer il suffit d'executer le main au moins une fois.

--- NF20_HuffmannProject_Gagnaire_Daix_Peltier/src/main/benchmark/DecoderBenchmark.java
+++ NF20_HuffmannProject_Gagnaire_Daix_Peltier/src/main/benchmark/DecoderBenchmark.java
@@ -1,5 +1,30 @@
 package main.benchmark;
 
-public class DecodeBenchmark {
+import main.file.FileProcessor;
 
+import com.google.caliper.Benchmark;
+import com.google.caliper.Param;
+
+public class DecoderBenchmark extends Benchmark {
+	private FileProcessor fileProcessor;
+	@Param ({
+		"src/main/ressources/emptyFile.bin",
+		"src/main/ressources/simpleFile.bin",
+		"src/main/ressources/mediumFile.bin",
+		"src/main/ressources/shitLoadOfTextFile.bin"
+	}) String url;
+
+	@Override
+	protected void setUp() throws Exception{
+		this.fileProcessor = new FileProcessor(false);
+	}
+
+	public int timeEncodeFileWithoutSaving(int reps) {
+		String url = this.url;
+		int dummyValue = 0;
+		for (int i = 0; i < reps; i++) {
+			dummyValue |= this.fileProcessor.decodeFileAt(url);
+		}
+		return dummyValue;
+	}
 }

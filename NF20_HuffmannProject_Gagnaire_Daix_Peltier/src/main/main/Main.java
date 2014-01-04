f1afb632a0e829694410eed576727b8b0217485b
florence <florence@David-laptop>
2014-01-04 17:14:50 +0100

    MAJ Ressource - plein de fichier en plus :)

--- NF20_HuffmannProject_Gagnaire_Daix_Peltier/src/main/main/Main.java
+++ NF20_HuffmannProject_Gagnaire_Daix_Peltier/src/main/main/Main.java
@@ -21,6 +21,12 @@ public class Main {
 	public static void main (String[] args) {
 		String url1 = "src/main/ressources/simpleFile.txt"; // instance "officiel" 1
 		String url2 = "src/main/ressources/mediumFile.txt"; // instance "officiel" 2
+		String url3 = "src/main/ressources/bigFile.txt"; 
+		String url4 = "src/main/ressources/ExtraBigFile.txt"; 
+		String url5 = "src/main/ressources/OverExtraBigFile.txt"; 
+		String url6 = "src/main/ressources/UltraOverExtraBigFile.txt"; 
+		String url7 = "src/main/ressources/shitFile.txt"; 
+
 		String urlToCompressedFile = "src/main/ressources/compressed.bin";
 		try {
 			//Encodage de tous les fichiers --sert pour le benchmark de décodage de Caliper
@@ -33,7 +39,7 @@ public class Main {
 			//System.out.println("----- Encoding only - Display -----");
 			//encodeFileWithoutSavingWithDebugDisplay(url1);
 			System.out.println("----- Encoding & Decoding - Display -----");
-			encodeAndThenDecondeFileWithDebugDisplay(url1,urlToCompressedFile);
+			encodeAndThenDecondeFileWithDebugDisplay(url2,urlToCompressedFile);
 		} catch (Exception e) {
 			System.err.println("========================================");
 			System.err.println("[ERROR] An error occured during runtime");
